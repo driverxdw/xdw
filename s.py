12768eccec47463a32129a19b455b58d41080d01
xdw123456 <973858433@qq.com>
2017-05-12 00:11:47 +0800

    xdw1234

---
+++ s.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+#encoding=utf-8
+print 'xdw,believe yuoself!'
---
+++ 水仙花数.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#encoding=utf-8
+for i in range (100,1000) :
+    if int(str(i)[0])**3+int(str(i)[1])**3+int(str(i)[2])**3==i:
+            print i
